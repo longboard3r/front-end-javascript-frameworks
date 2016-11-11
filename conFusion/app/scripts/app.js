19af85caa33d32c566ebaba469a460538ea1a58a
Joseph Barrowcliffe <j.barrowcliffe@qut.edu.au>
2016-11-11 16:16:50 +1000

    Angular forms and form validation

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -73,4 +73,21 @@ angular.module('confusionApp', [])
     $scope.toggleDetails = function() {
       $scope.showDetails = !$scope.showDetails;
     };
-}]);
+  }])
+
+  .controller('ContactController', ['$scope', function($scope) {
+
+    $scope.feedback = {
+      mychannel:"",
+      firstName:"",
+      lastName:"",
+      agree:false,
+      email:""
+    };
+  
+  }])
+
+  .controller('FeedbackController', ['$scope', function($scope) {
+
+  }]);
+;
