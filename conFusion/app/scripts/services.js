d1eed8719ddd57b2d1b0957f2711673ef04016a8
Joseph Barrowcliffe <j.barrowcliffe@qut.edu.au>
2016-12-02 15:00:44 +1000

    ngRoute and SPAs

--- conFusion/app/scripts/services.js
+++ conFusion/app/scripts/services.js
@@ -6,6 +6,7 @@ angular.module('confusionApp')
 
 			var dishes=[
                          {
+			    _id:0,
 			    name: 'Uthapizza',
 			    image: 'images/uthapizza.png',
 			    category: 'mains',
@@ -39,6 +40,7 @@ angular.module('confusionApp')
 			        date: "2011-12-02T17:57:28.556094Z"
 			    }]
 			}, {
+			    _id:1,
 			    name: 'Zucchipakoda',
 			    image: 'images/zucchipakoda.png',
 			    category: 'appetizer',
@@ -72,6 +74,7 @@ angular.module('confusionApp')
 			        date: "2011-12-02T17:57:28.556094Z"
 			    }]
 			}, {
+			    _id:2,
 			    name: 'Vadonut',
 			    image: 'images/vadonut.png',
 			    category: 'appetizer',
@@ -105,6 +108,7 @@ angular.module('confusionApp')
 			        date: "2011-12-02T17:57:28.556094Z"
 			    }]
 			}, {
+			    _id:3,
 			    name: 'ElaiCheese Cake',
 			    image: 'images/elaicheesecake.png',
 			    category: 'dessert',
