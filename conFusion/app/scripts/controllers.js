affaa422c80fe9f5160a541d3135dc5e8f4bef18
Joseph Barrowcliffe <j.barrowcliffe@qut.edu.au>
2016-12-06 13:56:30 +1000

    Assignment 3 - SPA

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -2,14 +2,15 @@
 
 angular.module('confusionApp')
 
-        .controller('MenuController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
             
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes = menuFactory.getDishes();
+            $scope.dishes= menuFactory.getDishes();
 
+                        
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
                 
@@ -53,7 +54,7 @@ angular.module('confusionApp')
                 
                 console.log($scope.feedback);
                 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
@@ -67,9 +68,9 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$routeParams','menuFactory', function($scope, $routeParams, menuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish = menuFactory.getDish(parseInt($routeParams.id,10));
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
             $scope.dish = dish;
             
@@ -77,33 +78,40 @@ angular.module('confusionApp')
 
         .controller('DishCommentController', ['$scope', function($scope) {
             
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            var comment = {                
-                author: "",
-                rating: 5,
-                comment: ""
-            };
-            
-            $scope.comment = comment;
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
             $scope.submitComment = function () {
                 
-                //Step 2: This is how you record the date
-                $scope.comment.date = new Date().toISOString();
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
                 
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push( $scope.comment);
+                $scope.dish.comments.push($scope.mycomment);
                 
-                //Step 4: reset your form to pristine
                 $scope.commentForm.$setPristine();
                 
-                //Step 5: reset your JavaScript object that holds your comment
-                 $scope.comment = {                
-                    author: "",
-                    rating: 5,
-                    comment: ""
-                 }; 
-            };
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            }
+        }])
+
+        // implement the IndexController 
+        .controller('IndexController', ['$scope', 'menuFactory','corporateFactory', function($scope, menuFactory, corporateFactory) {
+
+            var featuredDish = menuFactory.getDish(0);
+            $scope.featuredDish = featuredDish;
+
+            var promotion = menuFactory.getPromotion(0);
+            $scope.promotion = promotion;
+
+            var leader = corporateFactory.getLeader(3);
+            $scope.leader = leader;
+        }])
+
+        //and About Controller here
+        .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+
+            var leaders = corporateFactory.getLeaders();
+            $scope.leaders = leaders;
+           
         }])
 
 ;
